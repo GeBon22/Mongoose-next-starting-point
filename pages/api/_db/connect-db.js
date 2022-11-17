dbc81cc1d2f8d24a9ecfaa6efad45b5c225c17e5
Georg Bonin <114496506+GeBon22@users.noreply.github.com>
2022-11-17 17:00:02 +0100

    status quo without case "POST"

---
+++ pages/api/_db/connect-db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = (handler) => async (req, res) => {
+  if (mongoose.connections[0].readyState) {
+    // Use current db connection
+    return handler(req, res);
+  }
+  // Use new db connection
+  console.log(process.env.MONGO_DB_URI)
+  await mongoose.connect(process.env.MONGO_DB_URI);
+  return handler(req, res);
+};
+
+export default connectDB;
--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,18 +1,20 @@
-import { products } from "../_data/products";
+import connectDB from "../_db/connect-db";
+import { Product } from "../_db/models/Product";
 
-export default function handler(req, res) {
+async function handler(req, res) {
   switch (req.method) {
     case "GET":
-      const filteredProducts = req.query.category
-        ? products.filter((product) => product.category === req.query.category)
-        : products;
-      return res.status(200).json(
-        filteredProducts.map((p) => {
-          const { detail, ...rest } = p;
-          return rest;
-        })
-      );
+      try{
+      const products = await Product.find({});
+      return res.status(200).json(products);
+    } catch(error){
+      return res.status(500).json({error: error.message});
+    }
+    break;
+
     default:
       return res.status(400).json({ error: "method not supported" });
   }
 }
+
+export default connectDB(handler);
